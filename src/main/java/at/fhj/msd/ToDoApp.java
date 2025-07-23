9e379a417e6e3a99fa49cd838e9ebbeedbd2b9bf
maryam taeed <maryam.taeid@edu.fh-joanneum.at>
2025-07-23 13:46:54 +0200

    modify todoapp.java

--- src/main/java/at/fhj/msd/ToDoApp.java
+++ src/main/java/at/fhj/msd/ToDoApp.java
@@ -1,13 +1,36 @@
 package at.fhj.msd;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.io.*;
 
 public class ToDoApp {
     static ArrayList<String> tasks = new ArrayList<>();
     static Scanner input = new Scanner(System.in);
-    
+    static final String FILE_NAME="tasks.txt";
+    static void loadTasks(){
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line ;
+            while ((line = reader.readLine()) !=null){
+tasks.add(line);
+        
+}
+        } catch (IOException e) {
+            System.out.println("No previous tasks found" );
+        }
+
+    }
+    static void saveTasks(){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))){
+            for (String tasks :tasks){
+                writer.write(tasks);
+                writer.newLine();
+            }
+        }catch (IOException e) {
+            System.out.println("Error saving tasks: " );
+        }
+    }
     public static void main(String[] args) {
+        loadTasks();
         while (true) {
             System.out.println("\n1.Add Task\n2.show Tasks\n3.Remove Task\n4.Exit");
             String choice = input.nextLine();
@@ -27,6 +50,7 @@ public class ToDoApp {
   static void addTask() {
         System.out.print("Enter task: ");
         tasks.add(input.nextLine());
+        saveTasks();
   }
     static void showTasks() {
         System.out.println("\nYour tasks:");
@@ -45,5 +69,6 @@ public class ToDoApp {
         else {
             System.out.println("Invalid task number.");
         }
+        saveTasks();
     }
 }
